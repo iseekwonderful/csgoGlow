85a4b0ae0df11223728720dd704d606746a344ec
Your Name Here <you@yourdomain.com>
2016-05-01 23:54:10 +0800

    updated

--- External/External/main.c
+++ External/External/main.c
@@ -173,7 +173,7 @@ void readPlayerPointAndHealth(mach_vm_address_t imgbase, uint32_t startAddress, 
     int m_iGlowIndex = 0xA2D0;
     uint32_t memoryAddress;
     int glowIndex;
-    int playerBase = 0x00EEFF14;
+    int playerBase = 0x00EF0F14;
     printf("----------updated----------\n");
     for (int i = 0; i < 0x60; i++) {
         if (readIntMam(csgo, current, imgbase + playerBase + 0x10 * i, &memoryAddress) == -1)//Entetiy address
@@ -210,14 +210,14 @@ void readPlayerPointAndHealth(mach_vm_address_t imgbase, uint32_t startAddress, 
 }
 
 void glowInfo(mach_vm_address_t imgbase, uint32_t * address){
-    int glowInfoOffset = 0x134E6F0;
+    int glowInfoOffset = 0x134F6F0;
     readUint32Mam(csgo, current, imgbase + glowInfoOffset, address);
 }
 
 void localbaseInformation(mach_vm_address_t imgbase, int * i_teamNum){
     // read localbaseStartaDDress
     uint32_t localBase;
-    readUint32Mam(csgo, current, imgbase + 0x00EEFF04 + 0x10, &localBase);
+    readUint32Mam(csgo, current, imgbase + 0x00EF0F04 + 0x10, &localBase);
     // read icrossHairID
     readUint32Mam(csgo, current, localBase + 0xe4, i_teamNum);
 }
