d6d77cedd44173804802b761286936d3f320efa4
Andre Kalisch <aka@Andres-MacBook-Pro.local>
2018-08-16 08:07:10 +0200

    fixed m_nGlowIndex for CSGO Update on 18.05.2018

--- Wall/main.cpp
+++ Wall/main.cpp
@@ -39,7 +39,7 @@ uint64_t glowInfoOffset;
 uint64_t LocalPlayerBase;
 uint64_t playerBase;
 
-uint64_t m_iGlowIndex       = 0xAC00;
+uint64_t m_iGlowIndex       = 0xAC10;
 bool statBool = true;
 
 struct Color {
