67b894b3d279e4be3e0f18b0aa0c85be5197e5e0
Nicolas Fricker <54084264+Fricker95@users.noreply.github.com>
2020-06-06 09:16:13 -0700

    Update Offsets.hpp

--- Wall/Offsets.hpp
+++ Wall/Offsets.hpp
@@ -3,6 +3,29 @@
 
 #pragma once
 
+//client.dylib
+uint64_t m_dwGlowManager;
+uint64_t m_dwEntityList;
+uint64_t m_dwLocalPlayer;
+uint64_t m_dwLocalPlayerAddress;
+
+uint64_t m_dwEntityStructSize					=0x20;
+uint64_t m_dwGlowStructSize						=0x40;
+uint64_t m_dwGlowColorStruct					=0x8;
+uint64_t m_dwGlowEnable							=0x28;
+uint64_t m_iGlowIndex							=0xAD48;
+uint64_t m_iHealth								=0x138;
+uint64_t m_iTeam								=0x12C;
+uint64_t m_bLifeState							=0x297;
+uint64_t m_dFlashAlpha							=0xAD2C;
+uint64_t m_fFlashDuration						=0xAD30;
+uint64_t m_bDormant								=0x125;
+uint64_t m_bSpotted								=0xECD;
+uint64_t m_bSpottedByMask						=0xF10;
+
+#endif /*Offsets_hpp*/
+
+/*
 const char* Ranks[] =
 {
     "Unranked",
@@ -28,28 +51,6 @@ const char* Ranks[] =
     "Supreme Master First Class",
     "The Global Elite"
 };
-
-
-//client.dylib
-uint64_t m_dwGlowManager;
-uint64_t m_dwEntityList;
-uint64_t m_dwLocalPlayer;
-uint64_t m_dwLocalPlayerAddress;
-
-uint64_t m_dwEntityStructSize					=0x20;
-uint64_t m_dwGlowStructSize						=0x40;
-uint64_t m_dwGlowColorStruct					=0x8;
-uint64_t m_dwGlowEnable							=0x28;
-uint64_t m_iGlowIndex							=0xAD48;
-uint64_t m_iHealth								=0x138;
-uint64_t m_iTeam								=0x12C;
-uint64_t m_bLifeState							=0x297;
-uint64_t m_dFlashAlpha							=0xAD2C;
-uint64_t m_fFlashDuration						=0xAD30;
-uint64_t m_bDormant								=0x125;
-uint64_t m_bSpotted								=0xECD;
-uint64_t m_bSpottedByMask						=0xF10;
-
-#endif /*Offsets_hpp*/
+*/
 
 
