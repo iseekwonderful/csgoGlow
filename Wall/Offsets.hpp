dca194f17b24265b79ebdaf5420153a0f7bf633a
Nicolas Fricker <54084264+Fricker95@users.noreply.github.com>
2020-07-05 14:07:37 -0700

    reduced mem->write operations

--- Wall/Offsets.hpp
+++ Wall/Offsets.hpp
@@ -10,30 +10,32 @@
 #ifndef Offsets_hpp
 #define Offsets_hpp
 
-class Offsets {
+struct sOffsets {
 public:
-	struct Client {
+	struct sClient {
 		uint64_t m_dwGlowManager;
-		uint64_t m_dwGlowObjectLoopStartAddress;
+		uint64_t m_dwGlowObjectLoopStartBase;
 		uint64_t m_dwEntityList;
 		uint64_t m_dwLocalPlayer;
-		uint64_t m_dwLocalPlayerAddress;
+		uint64_t m_dwLocalPlayerBase;
 		uint64_t m_dwPlayerResource;
 		uint64_t m_dwRadarBase;
 		
+		uint64_t m_dwEntityListSize					    = 0x40;
 		uint64_t m_dwEntityStructSize					= 0x20;
 		uint64_t m_dwGlowStructSize						= 0x40;
 		uint64_t m_dwGlowColorStruct					= 0x8;
+		uint64_t m_dwGlowCount							= 0x10;
 		uint64_t m_dwGlowEnable							= 0x28;
-		uint64_t m_iGlowIndex							= 0xAD68; //0xAD48 //0xAD2C
+		uint64_t m_iGlowIndex							= 0xAD68; // 0xAD48 // 0xAD2C
 		uint64_t m_bDormant								= 0x125;
 		uint64_t m_iTeam								= 0x12C;
 		uint64_t m_iHealth								= 0x138;
 		uint64_t m_bLifeState							= 0x297;
 		uint64_t m_bSpotted								= 0xECD;
 		uint64_t m_bSpottedByMask						= 0xF10;
-		uint64_t m_dFlashAlpha							= 0xAD4C; //0xAD2C
-		uint64_t m_fFlashDuration						= 0xAD50; //0xAD30
+		uint64_t m_dFlashAlpha							= 0xAD4C; // 0xAD2C
+		uint64_t m_fFlashDuration						= 0xAD50; // 0xAD30
 		
 		/*
 		 // radar base needs new sig
@@ -44,26 +46,27 @@ public:
 		 uint64_t m_szRadarName                  		= 0x16C;
 		 
 		 // player resource needs new sig
-		 uint64_t m_iKills                               = 0x1288;
-		 uint64_t m_iAssists                             = 0x138C;
-		 uint64_t m_iDeaths                              = 0x1490;
-		 uint64_t m_bConnected                           = 0x1594;
-		 uint64_t m_bHasDefuser                          = 0x1E51;
-		 uint64_t m_bHasHelmet                           = 0x1E92;
-		 uint64_t m_iArmor                               = 0x1ED8;
-		 uint64_t m_iCompetitiveRanking                  = 0x20E0;
-		 uint64_t m_iCompetitiveWins                     = 0x21E4;
+		 uint64_t m_iKills                              = 0x1288;
+		 uint64_t m_iAssists                            = 0x138C;
+		 uint64_t m_iDeaths                             = 0x1490;
+		 uint64_t m_bConnected                          = 0x1594;
+		 uint64_t m_bHasDefuser                         = 0x1E51;
+		 uint64_t m_bHasHelmet                          = 0x1E92;
+		 uint64_t m_iArmor                              = 0x1ED8;
+		 uint64_t m_iCompetitiveRanking                 = 0x20E0;
+		 uint64_t m_iCompetitiveWins                    = 0x21E4;
 		 */
 	} client;
 	
-	struct Engine {
+	struct sEngine {
 		uint64_t m_dwCEngineClient;
+		uint64_t m_dwCEngineClientBase;
 		//uint64_t m_szGameDirectory;
-		uint64_t m_dwIsInGame                           = 0x1a8; //0x198
+		uint64_t m_dwIsInGame                           = 0x1a8; // 0x198
 		//uint64_t m_dwFullUpdate                         = 0x204;
 		//uint64_t m_iLocalPlayer                         = 0x210;
 		//uint64_t m_szMapFile                            = 0x218;
-		uint64_t m_szMapName                            = 0x32C; //0x31C
+		uint64_t m_szMapName                            = 0x32C; // 0x31C
 		//uint64_t m_dwGetMaxClients                      = 0x3A0;
 		//uint64_t m_setViewAngle                         = 0x8E20;
 	} engine;
@@ -92,3 +95,4 @@ public:
 };
 
 #endif /*Offsets_hpp*/
+
--- Wall/Wall.hpp
+++ Wall/Wall.hpp
@@ -24,16 +24,27 @@
 #include <thread>
 
 class Wall {
-	struct Color{
-		float red;
-		float green;
-		float blue;
-		float alpha;
+	struct sGlowEntity {
+	public:
+		uint64_t entityPointer;
+		float r;
+		float g;
+		float b;
+		float a;
+		char unk1[0x10];
+		bool RenderWhenOccluded;
+		bool RenderWhenUnoccluded;
+		bool FullBloom;
+		char unk2[0x15];
+		
+		bool isValidGlowEntity() {
+			return entityPointer != 0x0;
+		}
 	};
 	
 	Process* g_cProc = nullptr;
 	MemMngr* mem = nullptr;
-	Offsets* offsets = nullptr;
+	sOffsets* off = nullptr;
 	
 	off_t engine_moduleLength = 0;
 	mach_vm_address_t engine_moduleStartAddress;
@@ -57,7 +68,7 @@ public:
 		
 		g_cProc = new Process;
 		mem = new MemMngr(g_cProc);
-		offsets = new Offsets;
+		off = new sOffsets;
 		
 		g_cProc->mainPid() = g_cProc->get("csgo_osx64");
 		
@@ -111,21 +122,21 @@ public:
 		
 		getEnginePointers();
 		
-		if (offsets->engine.m_dwCEngineClient != 0x0)
-			printf("Engine Client\t\t\t= %s0x%llx%s\n", cT::getColor(cT::fG::green).c_str(), offsets->engine.m_dwCEngineClient, cT::getStyle(cT::sT::bold).c_str());
+		if (off->engine.m_dwCEngineClient != 0x0)
+			printf("Engine Client\t\t\t= %s0x%llx%s\n", cT::getColor(cT::fG::green).c_str(), off->engine.m_dwCEngineClient, cT::getStyle(cT::sT::bold).c_str());
 		
 		getClientPointers();
 		
-		if (offsets->client.m_dwLocalPlayer != 0x0) {
-			printf("Local Player Base\t\t= %s0x%llx%s\n", cT::getColor(cT::fG::green).c_str(), offsets->client.m_dwLocalPlayer, cT::getStyle(cT::sT::bold).c_str());
+		if (off->client.m_dwLocalPlayer != 0x0) {
+			printf("Local Player Base\t\t= %s0x%llx%s\n", cT::getColor(cT::fG::green).c_str(), off->client.m_dwLocalPlayer, cT::getStyle(cT::sT::bold).c_str());
 		}
 		
-		if (offsets->client.m_dwEntityList != 0x0) {
-			printf("Entity List\t\t\t= %s0x%llx%s\n", cT::getColor(cT::fG::green).c_str(), offsets->client.m_dwEntityList, cT::getStyle(cT::sT::bold).c_str());
+		if (off->client.m_dwEntityList != 0x0) {
+			printf("Entity List\t\t\t= %s0x%llx%s\n", cT::getColor(cT::fG::green).c_str(), off->client.m_dwEntityList, cT::getStyle(cT::sT::bold).c_str());
 		}
 			
-		if (offsets->client.m_dwGlowManager != 0x0) {
-			printf("Glow Manager\t\t\t= %s0x%llx%s\n", cT::getColor(cT::fG::green).c_str(), offsets->client.m_dwGlowManager, cT::getStyle(cT::sT::bold).c_str());
+		if (off->client.m_dwGlowManager != 0x0) {
+			printf("Glow Manager\t\t\t= %s0x%llx%s\n", cT::getColor(cT::fG::green).c_str(), off->client.m_dwGlowManager, cT::getStyle(cT::sT::bold).c_str());
 		}
 	}
 	
@@ -139,36 +150,39 @@ public:
 			delete g_cProc;
 		if (mem)
 			delete mem;
-		if (offsets)
-			delete offsets;
+		if (off)
+			delete off;
 	}
 	
 	void run(bool getOff = false) {
 		std::thread s_thread(&Wall::stopThread, this);
 		s_thread.detach();
 		
+		off->engine.m_dwCEngineClientBase = mem->read<uint64_t>(off->engine.m_dwCEngineClient);
+		 
 		int i_teamNum = 0;
 		
 		while (g_cProc->mainPid() != -1 && g_cProc->mainTask() != 0 && !stop.load()) {
-			offsets->client.m_dwLocalPlayerAddress = mem->read<uint64_t>(offsets->client.m_dwLocalPlayer);
-			if (offsets->client.m_dwLocalPlayerAddress != 0x0) {
-				i_teamNum = mem->read<int>(offsets->client.m_dwLocalPlayerAddress + offsets->client.m_iTeam);
-				if (offsets->client.m_dwGlowObjectLoopStartAddress == 0x0) {
-					offsets->client.m_dwGlowObjectLoopStartAddress = mem->read<uint64_t>(offsets->client.m_dwGlowManager);
-					if (offsets->client.m_dwGlowObjectLoopStartAddress != 0x0) {
-						printf("Glow Object Start\t\t= %s0x%llx%s\n", cT::getColor(cT::fG::green).c_str(),  offsets->client.m_dwGlowObjectLoopStartAddress, cT::getStyle(cT::sT::bold).c_str());
-					} else {
-						//getEnginePointers();
-						getClientPointers();
+			if (mem->read<int>(off->engine.m_dwCEngineClientBase + off->engine.m_dwIsInGame) == 6) {
+				off->client.m_dwLocalPlayerBase = mem->read<uint64_t>(off->client.m_dwLocalPlayer);
+				if (off->client.m_dwLocalPlayerBase != 0x0) {
+					i_teamNum = mem->read<int>(off->client.m_dwLocalPlayerBase + off->client.m_iTeam);
+					if (off->client.m_dwGlowObjectLoopStartBase == 0x0) {
+						off->client.m_dwGlowObjectLoopStartBase = mem->read<uint64_t>(off->client.m_dwGlowManager);
+						if (off->client.m_dwGlowObjectLoopStartBase != 0x0) {
+							printf("Glow Object Loop Start\t\t= %s0x%llx%s\n", cT::getColor(cT::fG::green).c_str(),  off->client.m_dwGlowObjectLoopStartBase, cT::getStyle(cT::sT::bold).c_str());
+						} else {
+							getEnginePointers();
+							getClientPointers();
+						}
 					}
+					if (getOff) {
+						getOffsets();
+						stop.store(true);
+					}
+					applyEntityGlow(i_teamNum);
 				}
-				if (getOff) {
-					getOffsets();
-					stop.store(true);
-				}
-				applyEntityGlow(i_teamNum);
 			}
-			
 			g_cProc->mainPid() = g_cProc->get("csgo_osx64");
 			g_cProc->mainTask() = g_cProc->task(g_cProc->mainPid());
 			usleep(refreshRate); // 800
@@ -190,58 +204,64 @@ private:
 	}
 	
 	void applyEntityGlow(int iTeamNum) {
-		Color color;
+		sGlowEntity glow;
 		uint64_t entityAddress;
 		uint64_t glowBase;
 		int health;
 		
-		for (int i = 0; i < 64; ++i){
-			entityAddress = mem->read<uint64_t>(offsets->client.m_dwEntityList + (offsets->client.m_dwEntityStructSize * i));
+		for (int i = 0; i < off->client.m_dwEntityListSize; ++i){
+			
+			entityAddress = mem->read<uint64_t>(off->client.m_dwEntityList + (off->client.m_dwEntityStructSize * i));
 			
 			if (entityAddress <= 0x0)
 				continue;
 			
-			int team = mem->read<int>(entityAddress + offsets->client.m_iTeam);
+			int team = mem->read<int>(entityAddress + off->client.m_iTeam);
 			
-			if (noTeammates && entityAddress != offsets->client.m_dwLocalPlayerAddress && team == iTeamNum)
+			if (noTeammates && entityAddress != off->client.m_dwLocalPlayerBase && team == iTeamNum)
 				continue;
 			
-			if (!mem->read<bool>(entityAddress + offsets->client.m_bDormant) && !mem->read<bool>(entityAddress + offsets->client.m_bLifeState)) {
+			if (!mem->read<bool>(entityAddress + off->client.m_bDormant) && !mem->read<bool>(entityAddress + off->client.m_bLifeState)) {
 				
-				health = mem->read<int>(entityAddress + offsets->client.m_iHealth);
+				health = mem->read<int>(entityAddress + off->client.m_iHealth);
 				
 				if (health == 0)
 					health = 100;
 				
-				if (team != iTeamNum) {
-					// Enemy glow colors
-					color.red = float((100 - health)/100.0);
-					color.green = float((health)/100.0);
-					color.blue = 0.0f;
-					color.alpha = 0.6f;
-				} else {
-					// Teammates glow colors
-					color.red = float((100 - health)/100.0);
-					color.green = 0.0f;
-					color.blue = float((health)/100.0);
-					color.alpha = 0.5f;
-				}
-				
-				glowBase = offsets->client.m_dwGlowObjectLoopStartAddress + (offsets->client.m_dwGlowStructSize * mem->read<int>(entityAddress + offsets->client.m_iGlowIndex));
-				
-				// Enables Glow
+				glowBase = off->client.m_dwGlowObjectLoopStartBase + (off->client.m_dwGlowStructSize * mem->read<int>(entityAddress + off->client.m_iGlowIndex));
 				if (glowBase != 0x0) {
-					mem->write<bool>(glowBase + offsets->client.m_dwGlowEnable, true);
-					mem->write<Color>(glowBase + offsets->client.m_dwGlowColorStruct, color);
+					glow = mem->read<sGlowEntity>(glowBase);
+					
+					if (team != iTeamNum) {
+						// Enemy glow colors
+						glow.r = float((100 - health)/100.0);
+						glow.g = float((health)/100.0);
+						glow.b = 0.0f;
+						glow.a = 0.6f;
+					} else {
+						// Teammates glow colors
+						glow.r = float((100 - health)/100.0);
+						glow.g = 0.0f;
+						glow.b = float((health)/100.0);
+						glow.a = 0.6f;
+					}
+					
+					// Enables Glow
+					glow.RenderWhenOccluded = true;
+					glow.RenderWhenUnoccluded = false;
+					mem->write<sGlowEntity>(glowBase, glow);
 				}
 				
 				// Anti flash
-				if (offsets->client.m_dwLocalPlayerAddress == entityAddress && maxFlash != -1) {
-					if (mem->read<double>(entityAddress + offsets->client.m_dFlashAlpha) > maxFlash) {
-						mem->write(entityAddress + offsets->client.m_dFlashAlpha, maxFlash);
+				if (off->client.m_dwLocalPlayerBase == entityAddress && maxFlash != -1) {
+					if (mem->read<double>(entityAddress + off->client.m_dFlashAlpha) > maxFlash) {
+						mem->write<double>(entityAddress + off->client.m_dFlashAlpha, maxFlash);
 					}
 				}
 			}
+			
+			if (mem->read<uint64_t>(entityAddress + off->client.m_dwEntityStructSize - 0xe) == 0x0)
+				break;
 		}
 	}
 	
@@ -265,13 +285,13 @@ private:
 		
 		for (int j = 0; j < 64; ++j) {
 			for (int i = 0; i < 64; ++i){
-				entityAddress = mem->read<uint64_t>(offsets->client.m_dwEntityList + (offsets->client.m_dwEntityStructSize * i));
+				entityAddress = mem->read<uint64_t>(off->client.m_dwEntityList + (off->client.m_dwEntityStructSize * i));
 				
 				if (entityAddress <= 0x0 || entityAddress == 0)
 					continue;
 				
-				if (!mem->read<bool>(entityAddress + offsets->client.m_bDormant) && !mem->read<bool>(entityAddress + offsets->client.m_bLifeState))
-					glowIndexes.emplace_back(mem->read<int>(entityAddress + offsets->client.m_iGlowIndex + j));
+				if (!mem->read<bool>(entityAddress + off->client.m_bDormant) && !mem->read<bool>(entityAddress + off->client.m_bLifeState))
+					glowIndexes.emplace_back(mem->read<int>(entityAddress + off->client.m_iGlowIndex + j));
 			}
 			
 			for (int i = 1; i < glowIndexes.size(); ++i) {
@@ -284,9 +304,9 @@ private:
 			glowIndexes.clear();
 			
 			if (isValid) {
-				printf("\nuint64_t %s\t\t= %s0x%llx%s\n", cT::print("m_iGlowIndex", cT::fG::yellow).c_str(), cT::getColor(cT::fG::green).c_str(), offsets->client.m_iGlowIndex + j, cT::getStyle(cT::sT::bold).c_str());
-				printf("uint64_t %s\t\t= %s0x%llx%s\n", cT::print("m_dFlashAlpha", cT::fG::yellow).c_str(), cT::getColor(cT::fG::green).c_str(), offsets->client.m_dFlashAlpha + j, cT::getStyle(cT::sT::bold).c_str());
-				printf("uint64_t %s\t= %s0x%llx%s\n\n", cT::print("m_fFlashDuration", cT::fG::yellow).c_str(), cT::getColor(cT::fG::green).c_str(), offsets->client.m_fFlashDuration + j, cT::getStyle(cT::sT::bold).c_str());
+				printf("\nuint64_t %s\t\t= %s0x%llx%s\n", cT::print("m_iGlowIndex", cT::fG::yellow).c_str(), cT::getColor(cT::fG::green).c_str(), off->client.m_iGlowIndex + j, cT::getStyle(cT::sT::bold).c_str());
+				printf("uint64_t %s\t\t= %s0x%llx%s\n", cT::print("m_dFlashAlpha", cT::fG::yellow).c_str(), cT::getColor(cT::fG::green).c_str(), off->client.m_dFlashAlpha + j, cT::getStyle(cT::sT::bold).c_str());
+				printf("uint64_t %s\t= %s0x%llx%s\n\n", cT::print("m_fFlashDuration", cT::fG::yellow).c_str(), cT::getColor(cT::fG::green).c_str(), off->client.m_fFlashDuration + j, cT::getStyle(cT::sT::bold).c_str());
 				return;
 			}
 			usleep(1000);
@@ -301,7 +321,7 @@ private:
 	void getEnginePointers() {
 		Scanner* engineScanner = new Scanner(engine_moduleStartAddress, engine_moduleLength, mem);
 		
-		offsets->engine.m_dwCEngineClient = engine_moduleStartAddress + engineScanner->getPointer(
+		off->engine.m_dwCEngineClient = engine_moduleStartAddress + engineScanner->getPointer(
 																				  (Byte*)"\x55\x48\x89\xE5\x48\x8B\x00\x00\x00\x00\x00\x48\x83\x00\x00\x5D\xC3\x66\x66\x66\x66\x66\x66\x2E\x0F\x1F\x84\x00\x00\x00\x00\x00",
 																				  "xxxxxx?????xx??xxxxxxxxxxxxxxxxx",
 																				  0x7
@@ -313,42 +333,40 @@ private:
 	void getClientPointers() {
 		Scanner* clientScanner = new Scanner(client_moduleStartAddress, client_moduleLength, mem);
 		
-		offsets->client.m_dwLocalPlayer = client_moduleStartAddress + clientScanner->getPointer(
+		off->client.m_dwLocalPlayer = client_moduleStartAddress + clientScanner->getPointer(
 																				(Byte*)"\x89\xD6\x41\x89\x00\x49\x89\x00\x48\x8B\x1D\x00\x00\x00\x00\x48\x85\xDB\x74\x00",
 																				"xxxx?xx?xxx????xxxx?",
 																				0xB
 																				) + 0x4;
 		
-		offsets->client.m_dwEntityList = client_moduleStartAddress + clientScanner->getPointer(
+		off->client.m_dwEntityList = client_moduleStartAddress + clientScanner->getPointer(
 																			   (Byte*)"\x48\x8D\x1D\x00\x00\x00\x00\x48\x89\xDF\xE8\x00\x00\x00\x00\x48\x8D\x3D\x00\x00\x00\x00\x48\x8D\x15\x00\x00\x00\x00\x48\x89\xDE",
 																			   "xxx????xxxx????xxx????xxx????xxx",
 																			   0x3
 																			   ) + 0x2C;
 		
-		offsets->client.m_dwGlowManager = client_moduleStartAddress + clientScanner->getPointer(
+		off->client.m_dwGlowManager = client_moduleStartAddress + clientScanner->getPointer(
 																				(Byte*)"\x48\x8D\x3D\x00\x00\x00\x00\xE8\x00\x00\x00\x00\x85\xC0\x0F\x84\x00\x00\x00\x00\x48\xC7\x05\x00\x00\x00\x00\x00\x00\x00\x00\x48\x8D\x35\x00\x00\x00\x00",
 																				"xxx????x????xxxx????xxx????xxxxxxx????",
 																				0x22
 																				) + 0x4;
-		//offsets->client.m_dwGlowManager = client_moduleStartAddress + 0x1E24AF0;
-
 		/*
-		offsets->client. m_dwRadarBase = client_moduleStartAddress + clientScanner->getPointer(
+		off->client. m_dwRadarBase = client_moduleStartAddress + clientScanner->getPointer(
 		 (Byte*)"\x55\x48\x89\xE5\x41\x57\x41\x56\x41\x55\x41\x54\x53\x50\x49\x89\xFD\x49\x8B\x7D\x00\x48\x85\xFF\x74\x56\x41\x8B\x75\x00\xE8\x6D\x00\x00\x00\x41\x88\xC6\x45\x84\xF6\x75\x45\x41\x80\xF6\x00\x45\x8B\x65\x00\x45\x85\xE4\x7E\x3B\x31\xDB\x4C\x8D\x3D\x00\x00\x00\x00\x66\x66\x66\x66\x66\x66\x2E\x0F\x1F\x84\x00\x00\x00\x00\x00",
 		 "xxxxxxxxxxxxxxxxxxxx?xxxxxxxx?xxxxxxxxxxxxxxxx?xxx?xxxxxxxxxx????xxxxxxxxxxxxxxx",
 		 0x3D
 		 ) + 0x3C;
 		 
-		 printf("Radar Base: 0x%llx\n", offsets->client.m_dwRadarBase);
+		 printf("Radar Base: 0x%llx\n", off->client.m_dwRadarBase);
 		*/
 		/*
-		offsets->client. m_dwPlayerResource = client_moduleStartAddress + clientScanner->getPointer(
+		off->client. m_dwPlayerResource = client_moduleStartAddress + clientScanner->getPointer(
 		 (Byte*)"\x55\x48\x89\xE5\x41\x57\x41\x56\x41\x55\x41\x54\x53\x48\x83\xEC\x00\x48\x89\xF3\x48\x8D\x7D\xB0\xBE\x00\x00\x00\x00\xE8\xAE\x2F\xDC\xFF\x4C\x8B\x75\xD0\x48\x8B\x03\x48\x8B\x80\x00\x00\x00\x00\x48\x89\xDF\xFF\xD0\x41\x80\x4E\x00\x00\x49\x89\x46\x00\x48\x8D\x1D\x00\x00\x00\x00\x48\x8B\x3B\x48\x8B\x07\x48\x8B\x80\x00\x00\x00\x00\xFF\xD0\x4C\x8D\x25\x00\x00\x00\x00",
 		 "xxxxxxxxxxxxxxxx?xxxxxxxx????xxxxxxxxxxxxxxx????xxxxxxxx??xxx?xxx????xxxxxxxxx????xxxxx????",
 		 0x57
 		 ) + 0x4;
 		 
-		 printf("Player Resource: 0x%llx\n", offsets->client.m_dwPlayerResource);
+		 printf("Player Resource: 0x%llx\n", off->client.m_dwPlayerResource);
 		*/
 		
 		delete clientScanner;
