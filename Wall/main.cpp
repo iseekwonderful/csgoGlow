f18b510970ae715f132fb60fe617f401e3e061ce
Fricker95 <54084264+Fricker95@users.noreply.github.com>
2020-08-19 13:16:38 -0700

    Update main.cpp

--- Wall/main.cpp
+++ Wall/main.cpp
@@ -36,7 +36,7 @@ void usage(const char* exec) {
 	printf("\tsudo -s\n");
 	printf("\t%s [-f <max flash alpha>] [-r <refresh rate>] [-t] [-o] [-h]\n\n", cT::print(exec, cT::fG::yellow).c_str());
 	printf("\t-f <flash alpha>\t: Antiflash alpha max amount (default: 100.0, disable: -1, range: [0-2700])\n");
-	printf("\t-r <refresh rate>\t: Refresh rate in microseconds (default: 2000.0)\n");
+	printf("\t-r <refresh rate>\t: Refresh rate in microseconds (default: 1000.0)\n");
 	printf("\t-t\t\t\t: Disables teammate glow\n");
 	printf("\t-o\t\t\t: Get new offsets (only use with -insecure launch option flag in CSGO)\n");
 	printf("\t-h\t\t\t: Display this message\n\n");
